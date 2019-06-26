bb78dd425ad86f3186e06ae2b633ff36822adc4f
GproSoftware828 <JetSendProductions@Gmail.com>
2019-06-26 19:32:21 +1000

    Decorators Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import './controllers/LoginController';
+import './controllers/RootController';
 import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['1234567pawd'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
